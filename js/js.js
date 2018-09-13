2bfd0c2ffdca3c2ceb663239b4880115345d432c
= <=>
2018-09-13 21:57:46 +0700

    commit to pository

---
+++ js/js.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	$('.product .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+	})
+	$('.post-feature .owl-carousel').owlCarousel({
+    loop:true,
+    margin:25,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+	})
+});
